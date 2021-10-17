fb6e8bfbdc2e00454fe7431b150f2bf82139ddb1
HarshGG <ritzred2012@gmail.com>
2021-10-17 09:57:30 -0500

    updated

--- main_scraper.py
+++ main_scraper.py
@@ -9,11 +9,73 @@ from collections import OrderedDict
 import csv
 import random
 
-# def estProfit()
+def estProfit(name_cont, soup):
+    h3 = name_cont.find("h3")
+    extension = h3.find('a')['href']
+    URL2 = 'https://www.salvagebid.com' + extension
+    # Instantiate an Options object
+    # and add the "--headless" argument
+    PATH2 = "C:\Program Files (x86)\chromedriver.exe"
+    options2 = webdriver.ChromeOptions()
+    options2.add_argument("headless")
+    driver2 = webdriver.Chrome(PATH2, chrome_options=options2)
+    driver2.get(URL2)
+
+    time.sleep(5)
+    # print(URL2,"      EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
+
+    soup_file2=driver2.page_source
+    soup2 = BeautifulSoup(soup_file2)
+    sale_info = soup2.find('div',class_='lot-block sale-info')
+    # print(sale_info.prettify())
+    ul = sale_info.find_all('li', class_='lot-block-list-item')
+    x = 0
+    for li in ul:
+        print(x)
+        x+=1
+        print(li.prettify())
+        print("---")
+    cash_val_cont = ul[5]
+    cash_val_cont2 = ul[6]
+    # print(cash_val_cont)
+    cash_val_str = cash_val_cont.find('span').text.replace(',','')
+    cash_val_str2 = cash_val_cont2.find('span').text.replace(',','')
+    if(cash_val_str.isnumeric()):
+        # print(cash_val_str)
+        cash_val = int((cash_val_str[1:-3]))
+    elif(cash_val_str.isnumeric()):
+        cash_val = int((cash_val_str2[1:-3]))
+    else:
+        cash_val=0
+
+    est_repair_cont = ul[6]
+    est_repair_cont2 = None
+    try:
+        est_repair_cont2 = ul[7]
+    except(IndexError):
+        est_repair_cont2 = est_repair_cont
+    est_repair_str = est_repair_cont.find('span').text.replace(',','')
+    est_repair_str2 = est_repair_cont2.find('span').text.replace(',','')
+    if(est_repair_str.isnumeric()):
+        est_repair_val = int(est_repair_str[1:-3])
+    elif(est_repair_str2.isnumeric()):
+        est_repair_val = int(est_repair_str2[1:-3])
+    else:
+        est_repair_val = 0
+    print(est_repair_val)
+
+    if(est_repair_val==0):
+        if(not cash_val == 0):
+            est_repair_val = random.randint(cash_val-3000, cash_val+3000)
+        else:
+            cash_val = random.randint(10000,30000)
+            est_repair_val = random.randint(cash_val-3000, cash_val+3000)
+    driver2.close()
+    return cash_val - est_repair_val
 
 
 numData = "100"#input("Enter how many cars to collect data of: ")
-URL = "https://www.salvagebid.com/salvage-cars-for-sale?page=4&per_page="+numData+"&type=car"
+URL = "https://www.salvagebid.com/salvage-cars-for-sale?page=5&per_page="+numData+"&type=car"
 # Instantiate an Options object
 # and add the "--headless" argument
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -92,7 +154,7 @@ for rest_cont in result_containers:
         title = 'Other'
     the_small_dict['Title'] = title
 
-    # the_small_dict['Est. Profit'] = estProfit(rest_cont)
+    the_small_dict['Est. Profit'] = random.randint(-5000,5000)#estProfit(name_cont, soup)
     #TODO: reduce titles to salvage, clean, or none
 
     # location_cont = rest_cont.find('li', class_ = 'location')
@@ -104,13 +166,13 @@ for rest_cont in result_containers:
 
 #adding manual damage assessment
 # worth = input('Enter your opinion on the car\'s worth, based on damage assessment (separated by spaces) (out of 10): ').split()
-for w in range(len(the_big_dict)):
-    # if(w<len(worth)):
-    #     toAppend = round(float(worth[w]),1)
-    # else:
-    #     toAppend = round(random.random()*10,1)
-    toAppend = round(random.randint(1000,10000),0)
-    the_big_dict[w+1]['Worth'] = toAppend
+# for w in range(len(the_big_dict)):
+#     # if(w<len(worth)):
+#     #     toAppend = round(float(worth[w]),1)
+#     # else:
+#     #     toAppend = round(random.random()*10,1)
+#     toAppend = round(random.randint(1000,10000),0)
+#     the_big_dict[w+1]['Worth'] = toAppend
 #2.5 1.6 4.6 6.0 4.0 2.0 6.0 3.0 3.0 5.4 
 
 #sorting each sub_dict by key
@@ -119,7 +181,7 @@ for key in the_big_dict:
 
 pprint.pprint(the_big_dict)
 
-header = ['ID','Year', "Make", "Model", 'Odometer', 'Title','Damage', 'Engine','Worth']
+header = ['ID','Year', "Make", "Model", 'Odometer', 'Title','Damage', 'Engine','Est. Profit']
 
 toWrite = []
 
@@ -134,7 +196,7 @@ for x in range(1,len(the_big_dict)+1):
     toAppend.append(currDict['Title'])
     toAppend.append(currDict['Damage'])
     toAppend.append(currDict['Engine'])
-    toAppend.append(currDict['Worth'])
+    toAppend.append(currDict['Est. Profit'])
 
     toWrite.append(toAppend)
 
