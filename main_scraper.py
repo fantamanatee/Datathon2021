63fbd53ba00f44b11289c94e63c6290051626159
HarshGG <ritzred2012@gmail.com>
2021-10-17 03:00:51 -0500

    yeahhhhhhhhhhhhhhh

--- main_scraper.py
+++ main_scraper.py
@@ -4,8 +4,10 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+import time
 
-numData = input("Enter how many cars to collect data of: ")
+
+numData = "10"#input("Enter how many cars to collect data of: ")
 URL = "https://www.salvagebid.com/salvage-cars-for-sale?per_page="+numData+"&type=car"
 # Instantiate an Options object
 # and add the "--headless" argument
@@ -15,12 +17,18 @@ options.add_argument("headless")
 driver = webdriver.Chrome(PATH, chrome_options=options)
 driver.get(URL)
 
+time.sleep(5)
+
 soup_file=driver.page_source
 soup = BeautifulSoup(soup_file)
 body = soup.find('body')
 spa_react_root = soup.find(id="spa-react-root")
-page_content = spa_react_root.find_all("div", class_="page-content")
+page_content = spa_react_root.find("div", class_="page-content")
+# container = page_content.find("div",class_='container')
+vehicle_content = page_content.find("div",class_ = "vehicle-content")
+tester = spa_react_root.find("div",class_ = "result-item")
+
 
 
 print("EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE")
-print(spa_react_root.prettify())
+print(tester.prettify())
